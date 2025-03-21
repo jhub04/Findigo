7f30685c6b8ea4716cccae05e12a7b890a6f2542
jonathan <jonathhu@stud.ntnu.no>
2025-03-21 17:50:23 +0100

    Initial commit

---
+++ src/test/java/stud/ntnu/no/idatt2105/Findigo/FindigoApplicationTests.java
@@ -0,0 +1,13 @@
+package stud.ntnu.no.idatt2105.Findigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
