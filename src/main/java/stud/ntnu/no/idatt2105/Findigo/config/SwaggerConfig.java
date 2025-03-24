3c9c585eff9588627dcdaea3c1234b5021e5f2f1
Scott <scottld@stud.ntnu.no>
2025-03-24 15:48:15 +0100

    Added swagger config class

--- src/main/java/stud/ntnu/no/idatt2105/Findigo/config/SwaggerConfig.java
+++ src/main/java/stud/ntnu/no/idatt2105/Findigo/config/SwaggerConfig.java
@@ -1,4 +1,24 @@
 package stud.ntnu.no.idatt2105.Findigo.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .info(new Info()
+            .title("Findigo API")
+            .version("1.0")
+            .description("API for Findigo, the final project in IDATT2105 Full-stack applikasjonsutvikling")
+            .contact(new Contact()
+                .name("Scott du Plessis, Aryan Malekian, Mikael Stray Frøyshov, Jonathan Hubertz")
+                .email("scottld@ntnu.no")));
+  }
 }
