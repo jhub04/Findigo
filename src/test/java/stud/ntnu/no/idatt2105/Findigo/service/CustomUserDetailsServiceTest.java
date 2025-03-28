017cca93ab3fa5493e8ce83ece2d3b293a2f2a20
jonathan <jonathhu@stud.ntnu.no>
2025-03-28 14:15:17 +0100

    Change test username

--- src/test/java/stud/ntnu/no/idatt2105/Findigo/service/CustomUserDetailsServiceTest.java
+++ src/test/java/stud/ntnu/no/idatt2105/Findigo/service/CustomUserDetailsServiceTest.java
@@ -1,9 +1,11 @@
 package stud.ntnu.no.idatt2105.Findigo.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import stud.ntnu.no.idatt2105.Findigo.dtos.auth.RegisterRequest;
 
@@ -23,14 +25,14 @@ public class CustomUserDetailsServiceTest {
   @Test
   public void testLoadUserByUsername() {
     RegisterRequest registerRequest = new RegisterRequest();
-    registerRequest.setUsername("existingUser");
+    registerRequest.setUsername("existing");
     registerRequest.setPassword("password123");
 
-    assertThrows(UsernameNotFoundException.class, ()-> customUserDetailsService.loadUserByUsername("existingUser"));
+    assertThrows(UsernameNotFoundException.class, ()-> customUserDetailsService.loadUserByUsername("existing"));
     //Register user into system
     userService.register(registerRequest);
 
-    assertNotNull(customUserDetailsService.loadUserByUsername("existingUser"));
+    assertNotNull(customUserDetailsService.loadUserByUsername("existing"));
 
   }
 }
