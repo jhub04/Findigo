967229cd24a2f3e99a01a5a4db0e4733ece5ec33
Scott <scottld@stud.ntnu.no>
2025-03-28 09:49:42 +0100

    Added javadic to neww entity classes and added lat and lon to listing

--- src/main/java/stud/ntnu/no/idatt2105/Findigo/entities/Category.java
+++ src/main/java/stud/ntnu/no/idatt2105/Findigo/entities/Category.java
@@ -6,20 +6,43 @@ import lombok.Data;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents a category in the system, which can contain multiple attributes and listings.
+ * Each category has a unique name and is associated with multiple listings and attributes.
+ */
 @Data
 @Entity
 @Table(name = "category")
 public class Category {
+
+  /**
+   * The unique identifier for the category.
+   * This ID is automatically generated using the IDENTITY strategy.
+   */
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  /**
+   * The name of the category.
+   * This field must be unique and cannot be null.
+   */
   @Column(nullable = false, unique = true)
   private String categoryName;
 
+  /**
+   * The list of attributes associated with this category.
+   * Attributes define additional properties that listings in this category may have.
+   * If the category is deleted, all its attributes are also deleted due to CascadeType.ALL.
+   */
   @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
   private List<Attribute> attributes = new ArrayList<>();
 
+  /**
+   * The list of listings associated with this category.
+   * Listings represent individual items belonging to this category.
+   * If the category is deleted, all its listings are also deleted due to CascadeType.ALL.
+   */
   @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
   private List<Listing> listings = new ArrayList<>();
 }
