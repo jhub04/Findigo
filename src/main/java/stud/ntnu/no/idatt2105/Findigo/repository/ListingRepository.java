e072b8d62dfad8cac6d34595efd88cc3d502ed70
Scott <scottld@stud.ntnu.no>
2025-03-29 11:49:36 +0100

    Add functionality to get all listings from a cateogry

--- src/main/java/stud/ntnu/no/idatt2105/Findigo/repository/ListingRepository.java
+++ src/main/java/stud/ntnu/no/idatt2105/Findigo/repository/ListingRepository.java
@@ -2,6 +2,7 @@ package stud.ntnu.no.idatt2105.Findigo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import stud.ntnu.no.idatt2105.Findigo.entities.Category;
 import stud.ntnu.no.idatt2105.Findigo.entities.Listing;
 import stud.ntnu.no.idatt2105.Findigo.entities.User;
 
@@ -11,4 +12,5 @@ import java.util.List;
 public interface ListingRepository extends JpaRepository<Listing, Long> {
   //TODO javadoc
   List<Listing> findListingByUser(User user);
+  List<Listing> findListingsByCategoryId(Long id);
 }
