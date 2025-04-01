91f8a023e0da9f9b6343be7f7eb53e7f4d734725
Mikael Stray Frøyshov <mikaelsf@stud.ntnu.no>
2025-04-01 15:28:36 +0200

    feat(): implement UserLiteResponse: without own listings field

---
+++ src/main/java/stud/ntnu/no/idatt2105/Findigo/dtos/user/UserLiteResponse.java
@@ -0,0 +1,21 @@
+package stud.ntnu.no.idatt2105.Findigo.dtos.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import stud.ntnu.no.idatt2105.Findigo.dtos.listing.ListingResponse;
+import stud.ntnu.no.idatt2105.Findigo.entities.Listing;
+
+import java.util.List;
+
+/**
+ * Represents an authentication request data transfer object containing user credentials.
+ * This class is used when a user attempts to log in.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLiteResponse {
+  private Long id;
+  private String username;
+}
