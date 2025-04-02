ffd0f61ef75c2ff845dccef83845e533a247abd8
Mikael Stray Frøyshov <mikaelsf@stud.ntnu.no>
2025-04-02 13:32:34 +0200

    feat(mapper): implement toLiteDto method

--- src/main/java/stud/ntnu/no/idatt2105/Findigo/dtos/mappers/UserMapper.java
+++ src/main/java/stud/ntnu/no/idatt2105/Findigo/dtos/mappers/UserMapper.java
@@ -1,6 +1,10 @@
 package stud.ntnu.no.idatt2105.Findigo.dtos.mappers;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import stud.ntnu.no.idatt2105.Findigo.dtos.user.UserLiteResponse;
+import stud.ntnu.no.idatt2105.Findigo.dtos.user.UserRequest;
 import stud.ntnu.no.idatt2105.Findigo.dtos.user.UserResponse;
 import stud.ntnu.no.idatt2105.Findigo.entities.User;
 
@@ -11,8 +15,12 @@ import stud.ntnu.no.idatt2105.Findigo.entities.User;
  * that can be used in API responses.
  * </p>
  */
+
+@RequiredArgsConstructor
 @Component
 public class UserMapper {
+
+  private final PasswordEncoder passwordEncoder;
 
   /**
    * Converts a {@link User} entity to a {@link UserResponse} DTO.
@@ -26,5 +34,16 @@ public class UserMapper {
    */
   public UserResponse toDTO(User user) {
     return new UserResponse(user.getId(), user.getUsername(), user.getListings().stream().map(ListingMapper::toDto).toList());
+  }
+
+  public UserLiteResponse toLiteDto(User user) {
+    return new UserLiteResponse(user.getId(), user.getUsername());
+  }
+
+  public User toEntity(UserRequest request) {
+    return new User()
+            .setUsername(request.getUsername())
+            .setPassword(passwordEncoder.encode(request.getPassword()))
+            .setRoles(request.getRoles());
   }
 }
