21f69329907d9062f9f3dc345fe24eaccf188630
Mikael Stray Frøyshov <mikaelsf@stud.ntnu.no>
2025-04-04 16:17:34 +0200

    refactor(error): delete old custom exceptions

--- src/main/java/stud/ntnu/no/idatt2105/Findigo/exception/customExceptions/ImageDownloadException.java
+++
@@ -1,7 +0,0 @@
-package stud.ntnu.no.idatt2105.Findigo.exception.customExceptions;
-
-public class ImageDownloadException extends RuntimeException {
-  public ImageDownloadException(String message) {
-    super(message);
-  }
-}
