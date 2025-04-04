daafea211a3b52ae884e2a314cc55592fc0708a6
Mikael Stray Frøyshov <mikaelsf@stud.ntnu.no>
2025-04-04 17:48:56 +0200

    feat(): update error messages

--- src/main/java/stud/ntnu/no/idatt2105/Findigo/exception/CustomErrorMessage.java
+++ src/main/java/stud/ntnu/no/idatt2105/Findigo/exception/CustomErrorMessage.java
@@ -14,10 +14,13 @@ public enum CustomErrorMessage {
   LISTING_NOT_FOUND(404, "Listing not found."),
   CATEGORY_NOT_FOUND(404, "Category not found."),
   USERNAME_NOT_FOUND(404, "Username not found."),
+  ATTRIBUTE_NOT_FOUND(404, "Attribute not found"),
   LISTING_NOT_FOUND_IN_CATEGORY(404, "No listings found in the specified category."),
 
   // --- Unchanged ---
   CATEGORY_NAME_UNCHANGED(400, "The new category name must be different from the current name."),
+  ATTRIBUTE_NAME_UNCHANGED(400, "The new attribute name must be different from the current name."),
+
 
   // --- Image Errors ---
   IMAGE_UPLOAD_FAILED(500, "Failed to upload the image."),
