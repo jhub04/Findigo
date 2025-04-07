d88e07990e67570a5bdbf47e1a6c0ca582ec1d66
Scott <scottld@stud.ntnu.no>
2025-04-07 18:46:05 +0200

    Update gitignore and make own image directory for tests

---
+++ src/test/java/stud/ntnu/no/idatt2105/Findigo/service/MessageServiceTest.java
@@ -0,0 +1,9 @@
+package stud.ntnu.no.idatt2105.Findigo.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class MessageServiceTest {
+}
