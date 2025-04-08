04926d7812d94524a88efb4231261845ba068da1
Scott <scottld@stud.ntnu.no>
2025-04-08 12:18:29 +0200

    Add unit tests to recommendation service

--- src/main/java/stud/ntnu/no/idatt2105/Findigo/FindigoApplication.java
+++ src/main/java/stud/ntnu/no/idatt2105/Findigo/FindigoApplication.java
@@ -2,7 +2,6 @@ package stud.ntnu.no.idatt2105.Findigo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class FindigoApplication {
 	public static void main(String[] args) {
